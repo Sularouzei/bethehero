8b3a4a4c7413b98df394fe5b78f35053dfaf1cc3
Seu Nome <seu_email@example.comclear>
2024-10-16 17:57:31 -0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './global.css';
+
+import Routes from './routes';
+
+import Logon from './pages/Logon/index';
+
+function App() {
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
